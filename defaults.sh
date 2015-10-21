14077a4bb076b4abef44153aa16b3ba40db1b5dd
Lewis Marshall <lewis@technoplusit.co.uk>
2015-10-21 13:30:20 +0100

    Added multi-location support and uses DNS with TTL for proxied addresses

---
+++ defaults.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+export NGIX_CONF_DIR=/usr/local/openresty/nginx/conf
---
+++ lua/set_uuid.lua
@@ -0,0 +1,12 @@
+if os.getenv("ENABLE_UUID_PARAM") == "FALSE" then
+    return ""
+else
+    local socket = require("socket")
+    local uuid = require("uuid")
+    uuid.randomseed(socket.gettime()*10000)
+    local uuid_str = uuid()
+    ngx.var.uuidopt = uuid_str
+    ngx.var.uuid_log_opt = " nginxId=" .. uuid_str
+    local uuid_opt = "&nginxId=" .. uuid_str
+    return uuid_opt
+end
