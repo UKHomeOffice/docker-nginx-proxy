ac2edcf5e428566f5497cc57a581c0586f1adb60
Lewis Marshall <lewis@technoplusit.co.uk>
2015-11-11 13:26:37 +0000

    Added debug out of mock server tests

--- ci-build.sh
+++ ci-build.sh
@@ -80,9 +80,7 @@ echo "+++++++++++++++++"
 cd ./test-servers/
 ${SUDO_CMD} docker build -t mock-server-tag .
 cd ..
-${SUDO_CMD} docker run -d -P --name=mocking-server mock-server-tag
 
-sleep 5
 echo "=========="
 echo "TESTING..."
 echo "=========="
@@ -143,6 +141,13 @@ echo "Test access OK for /standards/... with client cert..."
      wget -O /dev/null --no-check-certificate https://${DOCKER_HOST_NAME}:${PORT}/standards/ \
      --certificate=./client_certs/client.crt \
      --private-key=./client_certs/client.key
+
+echo "May have to disbale Mock server tests (travis failing with linked server...)?"
+echo "Running mocking-server..."
+${SUDO_CMD} docker run -d -P --name=mockserver mockservertag
+echo "sleep 5..."
+sleep 5
+${SUDO_CMD} docker ps
 
 start_test "Start with Custom error pages redirect off" "${STD_CMD} \
            -e \"PROXY_SERVICE_HOST=mock-server\" \
