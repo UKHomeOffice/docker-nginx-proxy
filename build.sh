e3bc66499e9cd0e1f6637f2294def486d792d834
Giles Hinchcliff <gileshinchcliff@yahoo.com>
2016-05-25 10:27:01 +0100

    added basic auth and removed sysdig messages

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 set -e
 
 OPEN_RESTY_URL=http://openresty.org/download
-OPEN_RESTRY_VER=1.7.10.1
+OPEN_RESTRY_VER=1.9.7.4
 LUAROCKS_VER=2.2.1
 
 # Install all dependacies to build from source
@@ -23,7 +23,7 @@ yum -y install \
     wget
 
 # Prepare
-wget -O ngx_openresty-${OPEN_RESTRY_VER}.tar.gz ${OPEN_RESTY_URL}/ngx_openresty-${OPEN_RESTRY_VER}.tar.gz
+wget -O ngx_openresty-${OPEN_RESTRY_VER}.tar.gz ${OPEN_RESTY_URL}/openresty-${OPEN_RESTRY_VER}.tar.gz
 tar xzvf ngx_openresty-${OPEN_RESTRY_VER}.tar.gz
 rm ngx_openresty-${OPEN_RESTRY_VER}.tar.gz
 
@@ -36,7 +36,7 @@ unzip naxsi.zip
 rm naxsi.zip
 
 # Build!
-cd ngx_openresty-${OPEN_RESTRY_VER}
+cd openresty-${OPEN_RESTRY_VER}
 ./configure --add-module=../naxsi-master/naxsi_src --with-http_realip_module
 make
 make install
@@ -48,7 +48,7 @@ cp ./naxsi-master/naxsi_config/naxsi_core.rules  /usr/local/openresty/naxsi/
 
 cd luarocks-2.2.1
 ./configure --with-lua=/usr/local/openresty/luajit \
-    --lua-suffix=jit-2.1.0-alpha \
+    --lua-suffix=jit-2.1.0-beta1 \
     --with-lua-include=/usr/local/openresty/luajit/include/luajit-2.1
 make build
 make install
@@ -58,7 +58,7 @@ luarocks install luasocket
 cd -
 
 # Cleaning up source...
-rm -fr ngx_openresty-${OPEN_RESTRY_VER}
+rm -fr openresty-${OPEN_RESTRY_VER}
 rm -fr luarocks-${LUAROCKS_VER}
 rm -fr naxsi-master
 
--- go.sh
+++ go.sh
@@ -40,6 +40,27 @@ if [ "${NAME_RESOLVER}" == "" ]; then
         export NAME_RESOLVER=$(grep 'nameserver' /etc/resolv.conf | head -n1 | cut -d' ' -f2)
     fi
 fi
+# creates .htpasswd file from file
+if [ -z ${BASIC_AUTH+x} ]; then
+  echo "Basic Auth not set, skipping..."
+else
+  HTPASSWD=$(dirname ${BASIC_AUTH})
+  if [ -f $HTPASSWD ]; then #has the htpasswd file already been created.
+    echo "$HTPASSWD already created, skipping"
+  else
+    sed -i '/^$/d' ${BASIC_AUTH} #remove all empty lines.
+    while IFS= read line
+       do
+          #for every line in the file add user and password to .htpasswd
+
+          USER=$(echo $line | cut -d ":" -f 1 |  tr -d '[[:space:]]')
+          PASSWORD=$(echo $line | cut -d ":" -f 2 | tr -d '[[:space:]]') #remove whitespace from lines.
+          printf "$USER:$(openssl passwd -crypt $PASSWORD)\n" >> ${HTPASSWD}/.htpasswd
+       done < ${BASIC_AUTH}
+      rm ${BASIC_AUTH} #delete file now not needed
+  fi
+fi
+
 
 msg "Resolving proxied names using resolver:${NAME_RESOLVER}"
 echo "resolver ${NAME_RESOLVER};">${NGIX_CONF_DIR}/resolver.conf
@@ -86,7 +107,9 @@ fi
 case "${LOG_FORMAT_NAME}" in
     "json" | "text")
         msg "Logging set to ${LOG_FORMAT_NAME}"
-        echo "access_log /dev/stdout extended_${LOG_FORMAT_NAME};">${NGIX_CONF_DIR}/logging.conf
+        echo "map \$request_uri \$loggable { ~^/nginx_status/  0; default 1;}">${NGIX_CONF_DIR}/logging.conf #remove logging for the sysdig agent.
+
+        echo "access_log /dev/stdout extended_${LOG_FORMAT_NAME} if=\$loggable;" >> ${NGIX_CONF_DIR}/logging.conf
         ;;
     *)
         exit_error_msg "Invalid log format specified:${LOG_FORMAT_NAME}. Expecting json or text."
