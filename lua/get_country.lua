a36fa1f24f743bed0a2eb6fd018eab5b145948a9
Lewis Marshall <lewis@technoplusit.co.uk>
2016-07-06 10:25:18 +0100

    Fix country code lua script and add logging

--- lua/get_country.lua
+++ lua/get_country.lua
@@ -2,4 +2,4 @@ geoip = require("geoip")
 g = geoip.open_type("country")
 g = geoip.open("/usr/share/GeoIP/GeoLiteCountry.dat")
 r = g:lookup(ngx.arg[1])
-retrun r.country_code
+return r.country_code
