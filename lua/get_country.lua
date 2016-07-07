7e92c471af14040dc18de6a3fd23bcd9ffc57dd2
Lewis Marshall <lewis@technoplusit.co.uk>
2016-07-07 15:29:52 +0100

    Working with negative case

--- lua/get_country.lua
+++ lua/get_country.lua
@@ -32,9 +32,13 @@ local country_code = geoip.code_by_id(country_id)
 
 -- Work out if country is allowed:
 local allow_country_csv = os.getenv("ALLOW_COUNTRY_CSV")
-if not allow_country_csv == "" then
+local allowed_countries = {}
+allowed_countries = split(allow_country_csv, ",")
+if allow_country_csv == "" then
+    ngx.var.allowed_country = "NA"
+else
     ngx.var.allowed_country = "no"
-    for country in split(allow_country_csv, ",") do
+    for key,country in pairs(allowed_countries) do
         if country == country_code then
             ngx.var.allowed_country = "yes"
             break
