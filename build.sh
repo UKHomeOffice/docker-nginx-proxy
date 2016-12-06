8e97eb5c42530a5f18134150b2bf79d517a2b5d2
Giles Hinchcliff <gileshinchcliff@yahoo.com>
2016-12-06 13:57:06 +0000

    updated to add stub-status on localhost

--- build.sh
+++ build.sh
@@ -48,7 +48,8 @@ cd openresty-${OPEN_RESTY_VER}
 ./configure --add-module=../naxsi-0.54/naxsi_src \
             --add-module=../nginx-statsd-${STATSD_VER} \
             --with-http_realip_module \
-            --with-http_geoip_module
+            --with-http_geoip_module \
+            --with-http_stub_status_module
 make
 make install
 cd ..
