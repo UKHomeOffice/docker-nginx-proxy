07137601b172ceddd058af42a6f8763201057116
Robin Knipe <robin.knipe@gmail.com>
2017-07-27 16:41:41 +0100

    Extract the tag and push actions into a function

--- publish.sh
+++ publish.sh
@@ -5,8 +5,15 @@ BASE="/"
 NAME="nginx-proxy"
 FULL_NAME="${REPO}${BASE}${NAME}"
 DOCKER_USERNAME="lev-web-robot"
+
 docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}" "${REPO}"
-docker tag ngx "${FULL_NAME}:${DRONE_TAG}"
-docker tag ngx "${FULL_NAME}:latest"
-docker push "${FULL_NAME}:${DRONE_TAG}"
-docker push "${FULL_NAME}:latest"
+
+tag_n_push() {
+  echo "Publishing ${1} of ${NAME}..."
+  docker tag ngx "${FULL_NAME}:${1}"
+  docker push "${FULL_NAME}:${1}"
+  echo "published ${1}"
+}
+
+tag_n_push "${DRONE_TAG}"
+tag_n_push "latest"
