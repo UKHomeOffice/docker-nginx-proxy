be3973dd8f7229eaffe8f3055d12efc1ea4ee7af
Russell Howe <russell.howe@digital.cabinet-office.gov.uk>
2019-09-06 08:53:40 +0100

    Use binary_remote_addr for ratelimiting key
    By using a binary rather than a string representation of the remote address
    when keying into the ratelimiting buckets, we'll save memory and be more
    efficient.

--- enable_location.sh
+++ enable_location.sh
@@ -84,7 +84,7 @@ if [ "${REQS_PER_MIN_PER_IP}" != "" ]; then
     else
       unset burst_setting
     fi
-    echo "limit_req_zone \$remote_addr zone=reqsbuffer${LOCATION_ID}:10m rate=${REQS_PER_MIN_PER_IP}r/m;" \
+    echo "limit_req_zone \$binary_remote_addr zone=reqsbuffer${LOCATION_ID}:10m rate=${REQS_PER_MIN_PER_IP}r/m;" \
         >${NGIX_CONF_DIR}/nginx_rate_limits_${LOCATION_ID}.conf
     REQ_LIMITS="limit_req zone=reqsbuffer${LOCATION_ID} ${burst_setting};"
 fi
